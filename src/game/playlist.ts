33eb9132c089e3e75274c6bec51ccab8be0be176
Fagner Da Silva Pereira Filho <fagnersilvaout@gmail.com>
2026-07-15 12:39:29 -0300

    Update playlist.ts

--- src/game/playlist.ts
+++ src/game/playlist.ts
@@ -9,10 +9,10 @@ export type Track = {
 export const PLAYLIST: Track[] = [
   { title: "Papercut", artist: "Linkin Park", url: "/src/game/Papercut(MP3_160K).mp3" },
   { title: "Freak on a Leash", artist: "Korn", url: "/src/game/freak.mp3" },
-  { title: "Monster", artist: "Skillet", url: "/src/game/monster.mp3" },
+  { title: "Monster", artist: "Skillet", url: "/src/game/Monster (Alternate Radio Version)(MP3_160K).mp3" },
   { title: "For Whom the Bell Tolls", artist: "Metallica", url: "/src/game/bells.mp3" },
   { title: "I'm Not a Vampire", artist: "Falling in Reverse", url: "/src/game/vampire.mp3" },
-  { title: "Sunflower", artist: "Post Malone & Swae Lee", url: "/src/game/sunflower.mp3" },
+  { title: "Sunflower", artist: "Post Malone & Swae Lee", url: "/src/game/Post Malone_ Swae Lee - Sunflower (Spider-Man_ Into the Spider-Verse)(MP3_160K).mp3" },
   { title: "In the End", artist: "Linkin Park", url: "/src/game/intheend.mp3" },
   { title: "Crawling", artist: "Linkin Park", url: "/src/game/crawling.mp3" },
 ];
