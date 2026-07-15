dc97ccec5d2ec953da216db683d91994ad961a19
Fagner Da Silva Pereira Filho <fagnersilvaout@gmail.com>
2026-07-15 12:26:08 -0300

    Update playlist.ts

--- src/game/playlist.ts
+++ src/game/playlist.ts
@@ -7,12 +7,12 @@ export type Track = {
 // Coloque os MP3s em /public/musicas/ com estes nomes,
 // ou edite os campos `url` abaixo.
 export const PLAYLIST: Track[] = [
-  { title: "Papercut", artist: "Linkin Park", url: "/musicas/papercut.mp3" },
-  { title: "Freak on a Leash", artist: "Korn", url: "/musicas/freak.mp3" },
-  { title: "Monster", artist: "Skillet", url: "/musicas/monster.mp3" },
-  { title: "For Whom the Bell Tolls", artist: "Metallica", url: "/musicas/bells.mp3" },
-  { title: "I'm Not a Vampire", artist: "Falling in Reverse", url: "/musicas/vampire.mp3" },
-  { title: "Sunflower", artist: "Post Malone & Swae Lee", url: "/musicas/sunflower.mp3" },
-  { title: "In the End", artist: "Linkin Park", url: "/musicas/intheend.mp3" },
-  { title: "Crawling", artist: "Linkin Park", url: "/musicas/crawling.mp3" },
+  { title: "Papercut", artist: "Linkin Park", url: "/src/game/papercut.mp3" },
+  { title: "Freak on a Leash", artist: "Korn", url: "/src/game/freak.mp3" },
+  { title: "Monster", artist: "Skillet", url: "/src/game/monster.mp3" },
+  { title: "For Whom the Bell Tolls", artist: "Metallica", url: "/src/game/bells.mp3" },
+  { title: "I'm Not a Vampire", artist: "Falling in Reverse", url: "/src/game/vampire.mp3" },
+  { title: "Sunflower", artist: "Post Malone & Swae Lee", url: "/src/game/sunflower.mp3" },
+  { title: "In the End", artist: "Linkin Park", url: "/src/game/intheend.mp3" },
+  { title: "Crawling", artist: "Linkin Park", url: "/src/game/crawling.mp3" },
 ];
